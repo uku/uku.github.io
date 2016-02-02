65a5007418670fa8b386959aabdcd339c1000a9a
zhuzhuor <zhuzhuor@users.noreply.github.com>
2016-02-02 00:03:09 -0800

    use a link to replace the weibo button that doesn't work over https

--- js/script.js
+++ js/script.js
@@ -70,11 +70,11 @@ $(document).ready(function() {
     });
 
     // Learnt from http://mo2g.com/view/102/
-    jQuery(function($) {
-        $('script[data-url]').each(function() {
-            var _this = $(this),
-                url = _this.attr('data-url');
-            _this.attr('src', url);
-        });
-    });
+    //jQuery(function($) {
+    //    $('script[data-url]').each(function() {
+    //        var _this = $(this),
+    //            url = _this.attr('data-url');
+    //        _this.attr('src', url);
+    //    });
+    //});
 });
